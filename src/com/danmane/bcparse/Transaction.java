5393933aaa6d13ad8f8e703547d7e6ca37f3d752
Daniel Mané <danmane@gmail.com>
2013-04-25 01:24:45 -0500

    Placeholder for transaction parser

---
+++ src/com/danmane/bcparse/Transaction.java
@@ -0,0 +1,12 @@
+package com.danmane.bcparse;
+
+public class Transaction {
+	long memAddr;
+	long totalIn, totalOut, txFee;
+	int blockNum, numInputs, numOutputs;
+	
+	public Transaction(long memAddr, int blockNum, BlockChainBuffer bcb){
+		
+	}
+
+}
