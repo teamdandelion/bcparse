c6626e9c6e81c8e7de74459a0c3a36abeb23e38c
Daniel Mané <danmane@gmail.com>
2013-04-26 02:00:30 -0500

    Some basic functionality coded

---
+++ src/com/danmane/bcparse/ChainParser.java
@@ -0,0 +1,29 @@
+package com.danmane.bcparse;
+
+public class ChainParser {
+	BlockChainBuffer bcb;
+	int numBlocks;
+	
+	public ChainParser(String blkDirectory){
+		bcb = new BlockChainBuffer(blkDirectory);
+		
+	}
+	
+	public int getNumBlocks(){
+		long maxAddr = bcb.getMaxAddr();
+		long blockAddr = 0;
+		Block nextBlock;
+		numBlocks = 0;
+		while (blockAddr < maxAddr){
+			nextBlock = new Block(blockAddr, numBlocks, bcb);
+			numBlocks++;
+			blockAddr = nextBlock.getNextBlockAddr();
+		}
+		return numBlocks;
+	}
+	
+	public static void main(String[] args){
+		ChainParser cp = new ChainParser("/Users/danmane/Library/Application Support/Bitcoin/blocks/");
+		System.out.println(cp.getNumBlocks());
+	}
+}
