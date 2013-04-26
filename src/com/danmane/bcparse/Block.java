984b85fd43662b6976081456f41e448b59a6a310
Daniel Mané <danmane@gmail.com>
2013-04-26 02:00:13 -0500

    Block added, needs testing

--- src/com/danmane/bcparse/Block.java
+++ src/com/danmane/bcparse/Block.java
@@ -1,20 +1,45 @@
 package com.danmane.bcparse;
 
+import java.math.BigInteger;
+
 public class Block {
 	long memAddr, byteSize;
-	int blockNum;
-	long totalOut, totalFees;
-	int numTransactions; 
+	int blockNum, numTransactions, blockFormatVersion;
+	long totalOut, totalFees, blockTimestamp, blockLength;
+	byte[] blockHash, merkleRoot;
 	BlockChainBuffer theBuffer;
 	Transaction[] transactions;
-	// Spec allows for 2^64 transactions but I don't think it likely we'll see that..
-	byte[] blockHash;
+	static BigInteger magicNum = new BigInteger("4190024921");
 	
 	public Block(long memAddr, int blockNum, BlockChainBuffer theBuffer){
 		this.memAddr   = memAddr;
 		this.blockNum  = blockNum;
 		this.theBuffer = theBuffer;
 		
+		BigInteger shouldBeMagic = theBuffer.get4Byte(memAddr);
+		if (!shouldBeMagic.equals(magicNum)){
+			System.out.println("Block " + blockNum + " missing magic at addr " + memAddr);
+			throw new RuntimeException();
+			// All blocks are preceded by the 4 byte magic number
+		}
+		blockLength = theBuffer.get4Byte().longValue();
+		blockFormatVersion = theBuffer.get4Byte().intValue();
+		// Makes assumptions about length and format value that don't match spec,
+		// but seem reasonable
+		blockHash = theBuffer.get32Byte();
+		merkleRoot = theBuffer.get32Byte();
+		blockTimestamp = theBuffer.get4Byte().longValue();
+//		target = theBuffer.get4Byte();
+//		nonce = theBuffer.get4Byte();
+		numTransactions = theBuffer.getVarInt().intValue();
+		// Note spec allows for 2^64 transactions
+		
+		// TODO: Parse transactions
+		
+	}
+	
+	public long getNextBlockAddr(){
+		return memAddr + 8 + blockLength;
 	}
 	
 	public void writeToDB(BitcoinDB theDB){
