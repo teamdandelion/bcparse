2c4da88b915696bb7b7ad5e917b34ad123907447
Daniel Mané <danmane@gmail.com>
2013-04-28 20:39:18 -0500

    Not even sure if ill use this class

--- src/com/danmane/bcparse/BitcoinDB.java
+++ src/com/danmane/bcparse/BitcoinDB.java
@@ -1,5 +1,14 @@
 package com.danmane.bcparse;
 
-public class BitcoinDB {
+import org.iq80.leveldb.*;
+import static org.fusesource.leveldbjni.JniDBFactory.*;
+import java.io.*;
 
+public class BitcoinDB {
+	DB db;
+	public BitcoinDB(String fileName){
+		Options options = new Options();
+		options.createIfMissing(true);
+//		db = factory.open(new File(fileName), options);
+	}
 }
